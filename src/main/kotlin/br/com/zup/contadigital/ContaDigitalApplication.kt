2c139a8c1621d23ac8a04db804523d01de66c489
Alison <alison.mota@zup.com.br>
2021-10-05 09:51:41 -0300

    Incluindo o projeto

---
+++ src/main/kotlin/br/com/zup/contadigital/ContaDigitalApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.contadigital
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContaDigitalApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContaDigitalApplication>(*args)
+}
