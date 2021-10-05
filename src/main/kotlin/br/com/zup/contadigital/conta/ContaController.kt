233eced0b98f876db83ad8b93ad05c53d32d97ff
Alison <alison.mota@zup.com.br>
2021-10-05 11:30:01 -0300

    Changes by BRENO N B NOCCIOLI

--- src/main/kotlin/br/com/zup/contadigital/conta/ContaController.kt
+++ src/main/kotlin/br/com/zup/contadigital/conta/ContaController.kt
@@ -1,7 +1,14 @@
 package br.com.zup.contadigital.conta
 
 import org.springframework.stereotype.Controller
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import javax.validation.Valid
 
+@Validated
 @Controller
 class ContaController {
 
