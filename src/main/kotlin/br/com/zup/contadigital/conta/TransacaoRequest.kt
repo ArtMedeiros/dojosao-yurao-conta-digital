d84e9f283b0958e57a9da12b4b08509e8dfda29f
Marcos Antenor <marcos.souza@zup.com.br>
2021-10-06 15:15:00 -0300

    feature: modificando a classe TransacaoRequest - teste com property

--- src/main/kotlin/br/com/zup/contadigital/conta/TransacaoRequest.kt
+++ src/main/kotlin/br/com/zup/contadigital/conta/TransacaoRequest.kt
@@ -5,8 +5,20 @@ import java.math.BigDecimal
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
-data class TransacaoRequest (
-        @field:ExistsById(fieldName = "id", entityClass = Conta::class) val idConta: Long,
-        @field:Positive @field:NotNull val valor: BigDecimal,
-        val idCliente: String,
+class TransacaoRequest (
+        val idConta: Long,
+        val valor: BigDecimal
         )
+{
+
+        val idClient: String
+                get() {
+                        TODO()
+                }
+
+//        constructor(idClient: String, valor: BigDecimal, request: TransacaoRequest) : this(request.idConta, valor)
+}
+
+//class Request(val idConta: Long, valor: BigDecimal) {
+//        constructor(idClient: String, valor: BigDecimal, request: Request) : this(request.idConta, valor)
+//}
