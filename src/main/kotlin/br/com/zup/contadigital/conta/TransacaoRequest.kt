8d5a0a0527f348e6f5002c0915f9dbf1ed7390dd
Alison <alison.mota@zup.com.br>
2021-10-07 15:20:36 -0300

    Correção do construtor secundário da classe TransacaoRequest.kt

--- src/main/kotlin/br/com/zup/contadigital/conta/TransacaoRequest.kt
+++ src/main/kotlin/br/com/zup/contadigital/conta/TransacaoRequest.kt
@@ -1,23 +1,15 @@
 package br.com.zup.contadigital.conta
 
-import br.com.zup.contadigital.compartilhado.validadores.ExistsById
 import java.math.BigDecimal
-import javax.validation.constraints.NotNull
-import javax.validation.constraints.Positive
 
-data class TransacaoRequest (
-        val idConta: Long,
-        val valor: BigDecimal
-        )
-{
+data class TransacaoRequest(
+    val idConta: Long,
+    val valor: BigDecimal
+) {
 
-        var idCliente: String? = "Esse é o id do cliente"
+    var idCliente: String? = "Esse é o id do cliente"
 
-        constructor(idCliente: String, idConta: Long, valor: BigDecimal) : this(idConta, valor){
-                this.idCliente = idCliente
-        }
+    constructor(idCliente: String, idConta: Long, valor: BigDecimal) : this(idConta, valor) {
+        this.idCliente = idCliente
+    }
 }
-
-//class Request(val idConta: Long, valor: BigDecimal) {
-//        constructor(idClient: String, valor: BigDecimal, request: Request) : this(request.idConta, valor)
-//}
